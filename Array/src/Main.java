0b8c7b67cbafe102578e35e1883a0164807605cd
snake <824475386@qq.com>
2019-07-15 22:47:55 +0800

    add stack

---
+++ Array/src/Main.java
@@ -0,0 +1,31 @@
+/**
+ * @program: Array
+ * @description: main函数
+ * @author: snake
+ * @create: 2019-07-11 20:46
+ */
+public class Main {
+    public static void main(String[] args) {
+        Array<Integer> array = new Array<>();
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+        }
+        System.out.println(array);
+
+        array.add(1,100);
+        System.out.println(array);
+
+        array.addFirst(-1);
+        System.out.println(array);
+
+        array.remove(2);
+        System.out.println(array);
+
+        array.removeElement(4);
+        System.out.println(array);
+
+        array.removeFirst();
+        System.out.println(array);
+
+    }
+}
---
+++ Stack/src/Stack.java
@@ -0,0 +1,14 @@
+/**
+ * @program: Data-Structural
+ * @description: 栈接口
+ * @author: snake
+ * @create: 2019-07-15 22:08
+ */
+public interface Stack<E> {
+
+    int getSize();
+    boolean isEmpty();
+    void push(E e);
+    E pop();
+    E peek();
+}
