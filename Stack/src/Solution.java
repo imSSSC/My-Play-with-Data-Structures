6e2c8c77564e57228c8fffbaafe1d9179c1aaba6
snake <824475386@qq.com>
2019-07-15 23:16:09 +0800

    添加了LeetCode 20题的解答

---
+++ Stack/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+/**
+ * @program: Data-Structural
+ * @author: snake
+ * @create: 2019-07-15 23:01
+ */
+public class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
